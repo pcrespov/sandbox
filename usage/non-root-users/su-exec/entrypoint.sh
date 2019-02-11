12509787f4604ce06eb6826111ba3a3d7c512901
Pedro Crespo <crespo@itis.ethz.ch>
2019-02-11 21:20:52 +0100

    Detecting docker group and group/user id of host and creating a user with same ids  Changes to be committed: 	modified:   su-exec/entrypoint.sh

--- usage/non-root-users/su-exec/entrypoint.sh
+++ usage/non-root-users/su-exec/entrypoint.sh
@@ -9,27 +9,34 @@
 #
 # See https://stackoverflow.com/questions/39397548/how-to-give-non-root-user-in-docker-container-access-to-a-volume-mounted-on-the
 
-
+# These volumes are expected mounted to host
 WHO=/tim
-
+DOCKER_SOCK=/var/run/docker.sock
 
 stat $WHO > /dev/null || \
-    (echo You must mount a file to "$WHO" in order to deduce the user &&\
+    (echo ERROR: You must mount a file to "$WHO" in order to deduce the user &&\
+     exit 1 )
+stat $DOCKER_SOCK > /dev/null || \
+    (echo ERROR: You must mount docker socket to "$DOCKER_SOCK" in order to deduce the group &&\
      exit 1 )
 
 
 # Idenfifies host's user and group id
 USERID=$(stat -c %u $WHO)
 GROUPID=$(stat -c %g $WHO)
+DOCKER_GROUPID=$(stat -c %g $DOCKER_SOCK)
 
+
+# Create a user with same user/group id and add in docker group
 deluser scu > /dev/null 2>&1
 addgroup -g $GROUPID scu
+addgroup -g $DOCKER_GROUPID scu
+
 adduser -u $USERID -G scu -D -s /bin/sh scu
 
 # in dev mode we give access to `scu` to host's mapped volumes
 #RUN addgroup -g $HOST_GID_ARG hgrp &&\
 #    addgroup scu hgrp && \
 #    chown -R scu:scu $HOME/.venv
-
 
 su-exec scu "$@"
