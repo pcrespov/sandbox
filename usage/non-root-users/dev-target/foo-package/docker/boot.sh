58719b0b20ce5722df69933949b1057d4d185626
Pedro Crespo <crespo@itis.ethz.ch>
2019-02-15 08:08:40 +0100

    Cleanup and doc

--- usage/non-root-users/dev-target/foo-package/docker/boot.sh
+++ usage/non-root-users/dev-target/foo-package/docker/boot.sh
@@ -3,28 +3,33 @@
 # BOOT ---------------------------------------------
 echo "Booting in ${MY_BOOT_MODE} mode ..."
 echo "  User    :`id $(whoami)`"
+
 echo "  Workdir :`pwd`"
+stat $(pwd) | grep 'Access: (' | sed 's/^/    /'
+ls -la $(pwd) | sed 's/^/    /'
+
+echo "  HOME :$HOME"
+stat $HOME | grep 'Access: (' | sed 's/^/    /'
+ls -la $HOME | sed 's/^/    /'
+
 if [[ ${MY_BOOT_MODE} == "development" ]]
-then 
-    ls -la | sed 's/^/    /'
+then
     # WARNING: might show secrets in logs
     echo "  Environment :" 
     printenv  | sed 's/=/: /' | sed 's/^/    /' | sort
 fi
 
 if [[ ${MY_BOOT_MODE} == "development" ]]
-then     
+then
     cd /devel/foo-package
-    $MY_PIP install -e .
+    $MY_PIP install --user -e .
 fi
-
 
 echo "  Python :" 
 python --version | sed 's/^/    /'
 which python | sed 's/^/    /'
 echo "  PIP :" 
 $MY_PIP list | sed 's/^/    /'
-
 
 
 # RUNNING application ----------------------------------------
